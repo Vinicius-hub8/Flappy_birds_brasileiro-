33ec2ca86d69721b1adc03b00e07b43dfba360cc
Vinícius Amaral <viniciusamaraloliveira8@gmail.com>
2025-06-11 17:59:45 -0300

    Definindo o tamanho da tela

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import random
 import os
 import tkinter as tk
 from tkinter import messagebox  
+from recursos import funcoes
+from recursos.funcoes import inicializarBancoDeDados
+pygame.init()
+inicializarBancoDeDados()
+tamanho = (1000,700)
+relogio = pygame.time.Clock()
+tela = pygame.display.set_mode( tamanho )  
+branco=(255,255,255)
+preto=(0,0,0)   
