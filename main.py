fdaed44af0c69433f378b38625dca997bce641a6
Vinícius Amaral <viniciusamaraloliveira8@gmail.com>
2025-06-11 21:03:35 -0300

    Confirmando

--- main.py
+++ main.py
@@ -7,27 +7,45 @@ from recursos import funcoes
 from recursos import funcoesnew
 from recursos.funcoes import inicializarBancoDeDados
 
-# Função para pedir o nome do jogador
-def pedir_nome():
-    root = tk.Tk()
-    root.withdraw()  # Esconde a janela principal
-    nome = simpledialog.askstring("Nome do Jogador", "Digite seu nome:")
-    root.destroy()
-    if not nome:
-        nome = "Jogador"
-    return nome
-
+tucano = pygame.image.load("assets/tucano.png")
+tucano_caindo= pygame.image.load("assets/tucano_caindo.png")
+tronco_cima = pygame.image.load("assets/tronco_cima.png")
+tronco_baixo = pygame.image.load("assets/tronco_baixo.png")
+fundo = pygame.image.load("assets/fundo do jogo.png")
+musica_fundo = pygame.mixer.Sound("assets/musica_fundo.wav")
+batida_tucano = pygame.mixer.Sound("assets/batida_tucano.wav")
+canto_tucano = pygame.mixer.Sound("assets/canto_tucano.wav")
 pygame.init()
 inicializarBancoDeDados()
-
-# Pede o nome antes de iniciar o jogo
-nome_jogador = pedir_nome()
-
 tamanho = (1000,700)
 relogio = pygame.time.Clock()
 tela = pygame.display.set_mode(tamanho)  
 branco = (255,255,255)
 preto = (0,0,0)   
+
+def jogar():
+    largura_janela = 300
+    altura_janela = 50
+    def obter_nome():
+        global nome
+        nome = entry_nome.get()  
+        if not nome:  
+            messagebox.showwarning("Aviso", "Por favor, digite seu nome!")  
+        else:
+            root.destroy()  
+
+
+ # Criação da janela principal
+    root = tk.Tk()
+    # Obter as dimensões da tela
+    largura_tela = root.winfo_screenwidth()
+    altura_tela = root.winfo_screenheight()
+    pos_x = (largura_tela - largura_janela) // 2
+    pos_y = (altura_tela - altura_janela) // 2
+    root.geometry(f"{largura_janela}x{altura_janela}+{pos_x}+{pos_y}")
+    root.title("Informe seu nickname")
+    root.protocol("WM_DELETE_WINDOW", obter_nome)
+
 
 # Configurações do jogador
 player_x = 100
